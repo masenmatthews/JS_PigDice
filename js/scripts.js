a0815f4a66e952248b3e345a8f5f95ceaad8bd9f
Masen Matthews and Maggie Summers <masen@>
2018-01-24 11:47:38 -0800

    attempt to add more js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,19 @@ function Game(name1, name2) {
   this.name2 = name2;
 }
 
+// function Dice(side1, side2, side3, side4, side5, side6) {
+//   this.side1 = side1;
+//   this.side2 = side2;
+//   this.side3 = side3;
+//   this.side4 = side4;
+//   this.side5 = side5;
+//   this.side6 = side6;
+// }
+
+function roll () {
+  var num = Math.floor(Math.random() * (6 - 0 + 1));
+}
+
 //ui
 $(document).ready(function() {
   $("form#name-input").submit(function(event) {
@@ -18,4 +31,14 @@ $(document).ready(function() {
     $("form#name-input").hide();
     $("#game-interface").fadeIn();
   });
+
+  $("button.roll").click(function()) {
+      event.preventDefault();
+  });
 });
+
+
+//var randomNumberBetween1and6 =
+
+  //});
+// });
