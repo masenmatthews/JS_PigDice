e97be23db1b799be7898084d5d227dc0f2f476dd
Masen Matthews and Maggie Summers <masen@>
2018-01-25 11:26:55 -0800

    add js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,24 @@
 //biz
-var diceValue = function() {
-  return Math.floor((Math.random() * 6) + 1);
-};
 
-function Player(turn) {
+function Player() {
   this.playerName = "";
   this.totalScore = 0;
   this.roundScore = 0;
-  this.roll = 0;
-  this.turn = turn;
+    //this.turn = turn;
 };
 
 Player.prototype.rollDice = function() {
-  if (this.roll === 1) {
-    this.roundScore === 0;
+  var diceValue = Math.floor((Math.random() * 6) + 1);
+  if (diceValue === 1) {
     alert("Switch players!");
+    this.roundScore === 0;
   } else {
-    return this.roundScore + this.totalScore;
+    return diceValue;
   }
 };
 
 Player.prototype.hold = function() {
-  return this.totalScore = this.roundScore + this.totalScore;
+  return this.roundScore + this.totalScore;
   alert("Switch players!");
 };
 
@@ -35,28 +32,30 @@ Player.prototype.win = function() {
 //ui
 $(document).ready(function() {
 
-  var newPlayer = new Player(player1, player2)
-
-  var player1 = "";
-  var player2 = "";
+  var player1 = new Player(name1);
+  var player2 = new Player(name2);
 
   $("form#name-input").submit(function(event) {
     event.preventDefault();
 
-    player1 = $("input#name1").val();
-    player2 = $("input#name2").val();
+    name1 = $("input#name1").val();
+    name2 = $("input#name2").val();
 
-    $("#welcome-message").text("Welcome to Pig Dice, " + player1 + " and " + player2 + "!");
+    $("#welcome-message").text("Welcome to Pig Dice, " + name1 + " and " + name2 + "!");
     $("form#name-input").hide();
     $("#game-interface").fadeIn();
   });
 
-
   $("button#roll1").click(function(event) {
     event.preventDefault();
     var rollOne = player1.rollDice();
-    console.log(rollDice());
-    $("ul#player-one-roll").append("<li>" + randomNum + "</li>");
+    $("ul#player-one-roll").append("<li>" + rollOne + "</li>");
+  });
+
+  $("button#hold1").click(function(event) {
+    event.preventDefault();
+    var holdOne = player1.hold();
+    $("ul#player-one-score").append("<li>" + holdOne + "</li>");
   });
 
   // $("button#roll2").click(function(event) {
@@ -66,10 +65,7 @@ $(document).ready(function() {
   //   $("ul#player-two-roll").append("<li>" + randomNum + "</li>");
   // });
   //
-  // $("button#hold1").click(function(event) {
-  //   event.preventDefault();
-  //   alert("Switch to Player 2!");
-  // });
+
   //
   // $("button#hold2").click(function(event) {
   //   event.preventDefault();
