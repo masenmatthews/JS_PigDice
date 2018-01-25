3f27af67a3b232371d4c5254cedb5a91a6481f41
Masen Matthews and Maggie Summers <maggie@>
2018-01-25 14:38:29 -0800

    streamline button output

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
 //biz
 
-function Player() {
+function Player(turn) {
   this.playerName = "";
   this.totalScore = [];
   this.roundScore = [];
-    //this.turn = turn;
+  this.turn = turn;
 };
 
 Player.prototype.rollDice = function() {
   var diceValue = Math.floor((Math.random() * 6) + 1);
   if (diceValue === 1) {
-    this.roundScore === 0;
+
+    alert("Next player's turn!");
+    //this.roundScore === 0;
   } else {
     this.roundScore.push(diceValue);
     console.log(this.roundScore);
@@ -22,6 +24,10 @@ Player.prototype.hold = function() {
   var sum = this.roundScore.reduce((total, amount) => total + amount);
   return sum + this.totalScore;
 };
+
+// Player.prototype.turn = function() {
+//
+// };
 
 Player.prototype.win = function() {
   if (this.totalScore >= 100) {
@@ -48,25 +54,30 @@ $(document).ready(function() {
     $("#game-interface").fadeIn();
   });
 
-  $("button#roll1").click(function(event) {
-    event.preventDefault();
+  $("button#roll1").click(function() {
     var rollOne = player1.rollDice();
+    $("ul#player-one-roll").text(rollOne);
   });
 
-  $("button#hold1").click(function(event) {
-    event.preventDefault();
+  $("button#hold1").click(function() {
     var holdOne = player1.hold();
-    $("ul#player-one-score").append("<li>" + holdOne + "</li>");
+    $("ul#player-one-score").text(holdOne);
   });
 
-  $("button#roll2").click(function(event) {
-    event.preventDefault();
-    var rollOne = player2.rollDice();
+  $("button#roll2").click(function() {
+    var rollTwo = player2.rollDice();
+    $("ul#player-two-roll").text(rollTwo);
   });
 
-  $("button#hold2").click(function(event) {
-    event.preventDefault();
+  $("button#hold2").click(function() {
     var holdOne = player2.hold();
-    $("ul#player-two-score").append("<li>" + holdOne + "</li>");
+    $("ul#player-two-score").text(holdOne);
+
+    //turn activation from biz logic
+
+    // if (diceValue === 1) {
+    // $(".well").hide();
+    //   // this.roundScore === 0;
+    // }
   });
 });
